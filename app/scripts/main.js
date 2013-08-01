c3fbfbb0b8bd08ab25342e716c64fafebc49e5b0
Kenji Saito <k.saito.1985@gmail.com>
2013-08-01 08:17:21 -0400

    improved the performance."

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,27 @@
 // shim layer with setTimeout fallback
-window.requestAnimFrame = (function(){
-    return  window.requestAnimationFrame       ||
+window.requestAnimFrame = (function () {
+    return  window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame    ||
-        function( callback ){
+        window.mozRequestAnimationFrame ||
+        function (callback) {
             window.setTimeout(callback, 1000 / 60);
         };
 })();
 
-(function(){
-    function initValue(){
-        for(var i = 0; i < side; i++){
+(function () {
+    function initValue() {
+        for (var i = 0; i < side; i++) {
             valUs[i] = [];
             valVs[i] = [];
 
             dU[ i ] = [];
             dV[ i ] = [];
 
-            for(var j = 0; j < side; j++){
-                if(i > side/7 * 3 && i < side/7*4 && j > side / 7 * 3 && j < side / 7 * 4){
+            for (var j = 0; j < side; j++) {
+                if (i > side / 9 * 4 && i < side / 9 * 5 && j > side / 9 * 4 && j < side / 9 * 5) {
                     valUs[i][j] = 0.5;
                     valVs[i][j] = 0.25;
-                }else{
+                } else {
                     valUs[i][j] = 1;
                     valVs[i][j] = 0;
                 }
@@ -60,7 +60,7 @@ window.requestAnimFrame = (function(){
     offset[0][0] = side - 1;
     offset[0][1] = 1;
 
-    for(i = 1; i < side - 1; i++){
+    for (i = 1; i < side - 1; i++) {
         offset[i] = [];
         offset[i][0] = i - 1;
         offset[i][1] = i + 1;
@@ -70,12 +70,12 @@ window.requestAnimFrame = (function(){
     offset[side - 1][0] = side - 2;
     offset[side - 1][1] = 0;
 
-    loop( );
+    loop();
 
-    function loop(){
-        for(var num = 0; num < 9; num++){
-            for(var i = 0; i < side; i++){
-                for(var j = 0; j < side; j++){
+    function loop() {
+        for (var num = 0; num < 9; num++) {
+            for (var i = 0; i < side; i++) {
+                for (var j = 0; j < side; j++) {
 
                     var u, v, left, right, up, down;
                     var uvv, lapU, lapV;
@@ -94,13 +94,13 @@ window.requestAnimFrame = (function(){
                     lapU = valUs[left][j] + valUs[right][j] + valUs[i][up] + valUs[i][down] - 4 * u;
                     lapV = valVs[left][j] + valVs[right][j] + valVs[i][up] + valVs[i][down] - 4 * v
 
-                    dU[i][j] = diffU*lapU  - uvv + paramF*(1 - u);
-                    dV[i][j] = diffV*lapV + uvv - (paramK+paramF)*v;
+                    dU[i][j] = diffU * lapU - uvv + paramF * (1 - u);
+                    dV[i][j] = diffV * lapV + uvv - (paramK + paramF) * v;
                 }
             }
 
-            for( i = 0; i < side; i++){
-                for( j = 0; j < side; j++){
+            for (i = 0; i < side; i++) {
+                for (j = 0; j < side; j++) {
                     valUs[i][j] += dU[i][j];
                     valVs[i][j] += dV[i][j];
                 }
@@ -108,15 +108,17 @@ window.requestAnimFrame = (function(){
         }
 
 
-
         context.fillStyle = "#000";
         context.fillRect(0, 0, side, side);
 
-        for( i = 0; i < side; i++){
-            for( j = 0; j < side; j++){
-                var color = ( 255 * ( 1 - valUs[i][j] ) ) | 0;
-                context.fillStyle = "rgb( 0, 0, " + color + ")";
-                context.fillRect( i, j , 1, 1)
+
+        for (i = 0; i < side; i++) {
+            for (j = 0; j < side; j++) {
+                if (valUs[i][j] < 1) {
+                    var color = ( 255 * ( 1 - valUs[i][j] ) ) | 0;
+                    context.fillStyle = "rgb( 0, 0, " + color + ")";
+                    context.fillRect(i, j, 1, 1)
+                }
             }
         }
 
@@ -127,13 +129,13 @@ window.requestAnimFrame = (function(){
     }
 
     var count = 0;
-    $(window).mousedown(function(){
+    $(window).mousedown(function () {
 
         count = (count + 1) % 6;
-        switch (count){
+        switch (count) {
             case 0:
-                diffU  = 0.16;
-                diffV  = 0.08;
+                diffU = 0.16;
+                diffV = 0.08;
                 paramF = 0.035;
                 paramK = 0.06;
                 break;
